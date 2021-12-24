0f4e7c2b6417a65e9dc9cbf8be14371dabb4efdb
dante-signal31 <dante.signal31@gmail.com>
2021-12-24 09:58:04 +0100

    Ground brick ended.

---
+++ Assets/Scripts/GroundAppearance.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class GroundAppearance : MonoBehaviour, IBorder
+{
+    [SerializeField] private bool hasBorder=true;
+    [SerializeField] private ThingsOverGround thingsOverGround;
+    
+    [SerializeField] private SpriteRenderer groundSpriteRenderer;
+    [SerializeField] private Sprite groundNoBorderSprite;
+    [SerializeField] private Sprite groundBorderSprite;
+
+    private GroundRubbishAppearance _rubbishAppearance;
+    private bool _appearanceUpdateNeeded = false;
+
+    private void Awake()
+    {
+        _rubbishAppearance = gameObject.GetComponentInChildren<GroundRubbishAppearance>();
+    }
+
+    private void Start()
+    {
+        UpdateAppearance();
+    }
+
+    /// <summary>
+    /// When a value changes on Inspector, mark brick to update appearance.
+    /// </summary>
+    private void OnValidate()
+    {
+        _appearanceUpdateNeeded = true;
+    }
+
+    /// <summary>
+    /// Update appearance if needed.
+    /// </summary>
+    private void LateUpdate()
+    {
+        #if UNITY_EDITOR
+        if (_appearanceUpdateNeeded)
+        {
+            UpdateAppearance();
+            _appearanceUpdateNeeded = false; 
+        }
+        #endif
+    }
+
+
+    /// <summary>
+    /// Update this prefab sprites according to given fields.
+    /// </summary>
+    private void UpdateAppearance()
+    {
+        SetGround();
+        _rubbishAppearance.PlaceThingsOverGround(thingsOverGround);
+    }
+    
+    /// <summary>
+    /// Show ground border if this brick has one.
+    /// </summary>
+    private void SetGround()
+    {
+        if (hasBorder)
+        {
+            groundSpriteRenderer.sprite = groundBorderSprite;
+        }
+        else 
+        {
+            groundSpriteRenderer.sprite = groundNoBorderSprite;
+        }
+    }
+
+    public bool IsBorderShown()
+    {
+        return hasBorder;
+    }
+
+    public void ShowBorder(bool showIt)
+    {
+        hasBorder = showIt;
+    }
+}
---
+++ Assets/Scripts/GroundRubbishAppearance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class GroundRubbishAppearance : MonoBehaviour
+{
+
+
+    [SerializeField] private ThingsOverGroundSprites placeableThingsSprites;
+
+    [SerializeField] private SpriteRenderer thingsOverGroundSpriteRenderer;
+    [SerializeField] private SpriteRenderer thingsOverGroundOccluderSpriteRenderer;
+
+    /// <summary>
+    /// Show rubbish (or nothing) over parent ground parent.
+    /// </summary>
+    /// <param name="thing">Rubbish type to show.</param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public void PlaceThingsOverGround(ThingsOverGround thing)
+    {
+        OccludedSprite occludedThingSprite = thing switch
+        {
+            ThingsOverGround.Nothing => placeableThingsSprites["Nothing"],
+            ThingsOverGround.Bones => placeableThingsSprites["Bones"],
+            ThingsOverGround.Garbage => placeableThingsSprites["Garbage"],
+            _ => throw new ArgumentOutOfRangeException(nameof(thing), thing, null)
+        };
+        if (occludedThingSprite.main != null)
+        {
+            thingsOverGroundSpriteRenderer.sprite = occludedThingSprite.main;
+            thingsOverGroundSpriteRenderer.enabled = true;
+        }
+        else
+        {
+            thingsOverGroundSpriteRenderer.enabled = false;
+        }
+
+        if (occludedThingSprite.occluder != null)
+        {
+            thingsOverGroundOccluderSpriteRenderer.sprite = occludedThingSprite.occluder;
+            thingsOverGroundOccluderSpriteRenderer.enabled = true;
+        }
+        else
+        {
+            thingsOverGroundOccluderSpriteRenderer.enabled = false;
+        }
+    }
+}
