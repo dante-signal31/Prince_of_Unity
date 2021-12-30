9a6c31c1ec5c3d969a8cfba90dc247aabaea0925
dante-signal31 <dante.signal31@gmail.com>
2021-12-30 12:25:34 +0100

    Prince statemachine changes properly at fighting substatemachine. #2

--- Assets/Scripts/CommandController.cs
+++ Assets/Scripts/CommandController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Prince
@@ -11,6 +10,9 @@ namespace Prince
     /// </summary>
     public class CommandController : MonoBehaviour
     {
+        [Tooltip("Needed to set state flags depending on commands.")]
+        [SerializeField] private Animator stateMachine;
+        
         private CommandSequence _commandQueue = new CommandSequence();
 
         /// <summary>
@@ -24,18 +26,33 @@ namespace Prince
             {
                 case Command.CommandType.Action:
                     Debug.Log($"(CommandController) Executed Action at {Time.time}");
+                    stateMachine.SetTrigger("ActionPressed");
                     break;
                 case Command.CommandType.Block:
                     Debug.Log($"(CommandController) Executed Block at {Time.time}");
+                    stateMachine.SetTrigger("Block");
+                    break;
+                case Command.CommandType.Strike:
+                    Debug.Log($"(CommandController) Execute strike at {Time.time}");
+                    stateMachine.SetTrigger("Strike");
+                    break;
+                case Command.CommandType.WalkRightWithSword:
+                    Debug.Log($"(CommandController) Execute  walk right with sword at {Time.time}");
+                    stateMachine.SetTrigger("WalkRightWithSword");
+                    break;
+                case Command.CommandType.WalkLeftWithSword:
+                    Debug.Log($"(CommandController) Execute  walk left with sword at {Time.time}");
+                    stateMachine.SetTrigger("WalkLeftWithSword");
+                    break;
+                case Command.CommandType.Sheathe:
+                    Debug.Log($"(CommandController) Executed Sheathe at {Time.time}");
+                    stateMachine.SetTrigger("Sheathe");
                     break;
                 case Command.CommandType.Duck:
                     Debug.Log($"(CommandController) Executed Duck at {Time.time}");
                     break;
                 case Command.CommandType.Jump:
                     Debug.Log($"(CommandController) Executed Jump at {Time.time}");
-                    break;
-                case Command.CommandType.Sheathe:
-                    Debug.Log($"(CommandController) Executed Sheathe at {Time.time}");
                     break;
                 case Command.CommandType.Stop:
                     Debug.Log($"(CommandController) Executed Stop at {Time.time}");
