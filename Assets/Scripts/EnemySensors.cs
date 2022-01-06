d9da26692dbc6e29ea63875e959fe1123644cc51
dante-signal31 <dante.signal31@gmail.com>
2022-01-06 11:58:37 +0100

    Created an enemy sensor component for characters.

---
+++ Assets/Scripts/EnemySensors.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prince
+{
+    /// <summary>
+    /// Component to manage character sensors to detect enemies.
+    /// </summary>
+    public class EnemySensors : MonoBehaviour
+    {
+        [Tooltip("Needed to set EnemySeen flags when any enemy is detected.")]
+        [SerializeField] private Animator stateMachine;
+        [SerializeField] private Transform forwardSensorStart;
+        [SerializeField] private Transform forwardSensorEnd;
+        [SerializeField] private Transform rearSensorStart;
+        [SerializeField] private Transform rearSensorEnd;
+
+
+        private bool _isGuard;
+        private Vector2 _forwardSensorStartPosition;
+        private Vector2 _forwardSensorEndPosition;
+        private float _forwardSensorDistance;
+        private Vector2 _rearSensorStartPosition;
+        private Vector2 _rearSensorEndPosition;
+        private float _rearSensorDistance;
+
+        private GameObject _forwardEnemy;
+        private GameObject _rearEnemy;
+
+        public GameObject ForwardEnemy
+        {
+            get=> _forwardEnemy;
+            private set
+            {
+                _forwardEnemy = value;
+                if (value != null)
+                {
+                    stateMachine.SetBool("enemySeenForward", true);
+                }
+                else
+                {
+                    stateMachine.SetBool("enemySeenForward", false);
+                }
+                stateMachine.SetBool("enemySeen", EnemySeen);
+            }
+        }
+        
+        public GameObject RearEnemy {
+            get=> _rearEnemy;
+            private set
+            {
+                _rearEnemy = value;
+                if (value != null)
+                {
+                    stateMachine.SetBool("enemySeenRear", true);
+                }
+                else
+                {
+                    stateMachine.SetBool("enemySeenRear", false);
+                }
+                stateMachine.SetBool("enemySeen", EnemySeen);
+            }
+        }
+
+        public bool EnemySeenForward => ForwardEnemy != null;
+        public bool EnemySeenRear => RearEnemy != null;
+        
+        public bool EnemySeen
+        {
+            get => EnemySeenForward || EnemySeenRear;
+        }
+
+        enum SensorType
+        {
+            Forward,
+            Rear
+        };
+        
+        /// <summary>
+        /// UpdateAnimator flags that depend on character sensors.
+        /// </summary>
+        private void UpdateStateMachineFlags()
+        {
+            stateMachine.SetBool("enemySeen", EnemySeen);
+            stateMachine.SetBool("enemySeenForward", EnemySeenForward);
+            stateMachine.SetBool("enemySeenRear", EnemySeenRear);
+        }
+        
+        private void Awake()
+        {
+            _isGuard = gameObject.CompareTag("Guard");
+            _forwardSensorStartPosition = forwardSensorStart.position;
+            _forwardSensorEndPosition = forwardSensorEnd.position;
+            _forwardSensorDistance = Vector2.Distance(_forwardSensorStartPosition, _forwardSensorEndPosition);
+            _rearSensorStartPosition = rearSensorStart.position;
+            _rearSensorEndPosition = rearSensorEnd.position;
+            _rearSensorDistance = Vector2.Distance(_rearSensorStartPosition, _rearSensorEndPosition);
+            UpdateStateMachineFlags();
+        }
+
+        /// <summary>
+        /// Return opponent detected in front of character if any, or null otherwise.
+        /// </summary>
+        /// <returns>Opponent detected or null otherwise.</returns>
+        private GameObject DetectForward()
+        {
+            Vector2 rayDirection = _forwardSensorEndPosition - _forwardSensorStartPosition;
+            RaycastHit2D hit = Physics2D.Raycast(_forwardSensorStartPosition, 
+                                                rayDirection, 
+                                                _forwardSensorDistance, 
+                                                LayerMask.GetMask("Characters"));
+            return DetectOpponent(hit.collider);
+        }
+
+        /// <summary>
+        /// Return opponent detected if any at character back, or null otherwise.
+        /// </summary>
+        /// <returns>Opponent detected or null otherwise.</returns>
+        private GameObject DetectRear()
+        {
+            Vector2 rayDirection = _rearSensorEndPosition - _rearSensorStartPosition;
+            RaycastHit2D hit = Physics2D.Raycast(_rearSensorStartPosition, 
+                rayDirection, 
+                _rearSensorDistance, 
+                LayerMask.GetMask("Characters"));
+            return DetectOpponent(hit.collider);
+        }
+
+        /// <summary>
+        /// Return other GameObject if other belongs to an opponent.
+        ///
+        /// Player opponents are Guards and Player is Guards opponent.
+        /// </summary>
+        /// <param name="otherCollider">Character collider detected.</param>
+        /// <returns>Other GameObject if it is an opponent. Null if it is not an opponent.</returns>
+        private GameObject DetectOpponent(Collider2D otherCollider)
+        {
+            if (otherCollider != null)
+            {
+                string otherTag = otherCollider.gameObject.tag;
+                if (((_isGuard) && (otherTag.Equals("Player"))) ||
+                    ((!_isGuard) && (otherTag.Equals("Guard"))))
+                {
+                    return otherCollider.gameObject;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        private void FixedUpdate()
+        {
+            ForwardEnemy = DetectForward();
+            RearEnemy = DetectRear();
+        }
+
+        private void DrawSensor(SensorType sensorType)
+        {
+            float gizmoRadius = 0.2f;
+            Vector3 gizmoSize = new Vector3(gizmoRadius, gizmoRadius, gizmoRadius);
+            switch (sensorType)
+            {
+                case SensorType.Forward:
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawCube(forwardSensorStart.position, gizmoSize);
+                    Gizmos.DrawSphere(forwardSensorEnd.position, gizmoRadius);
+                    break;
+                case SensorType.Rear:
+                    Gizmos.color = Color.cyan;
+                    Gizmos.DrawCube(rearSensorStart.position, gizmoSize);
+                    Gizmos.DrawSphere(rearSensorEnd.position, gizmoRadius);
+                    break;
+            }
+            Gizmos.color = sensorType switch
+            {
+                SensorType.Forward => (EnemySeenForward) ? Color.green : Color.red,
+                SensorType.Rear => (EnemySeenRear) ? Color.green : Color.red,
+            };
+            switch (sensorType)
+            {
+                case SensorType.Forward:
+                    Gizmos.DrawLine(forwardSensorStart.position, forwardSensorEnd.position);
+                    break;
+                case SensorType.Rear:
+                    Gizmos.DrawLine(rearSensorStart.position, rearSensorEnd.position);
+                    break;
+            }
+        }
+        
+        private void OnDrawGizmosSelected()
+        {
+            #if UNITY_EDITOR
+            DrawSensor(SensorType.Forward);
+            DrawSensor(SensorType.Rear);
+            #endif
+        }
+    }
+}
--- Assets/Tests/PlayTests/CharacterMovementTests.cs
+++ Assets/Tests/PlayTests/CharacterMovementTests.cs
@@ -11,7 +11,9 @@ namespace Tests.PlayTests
 {
     public class CharacterMovementTests
     {
-
+        private GameObject _prince;
+        private GameObject _enemy;
+        
         [UnitySetUp]
         public IEnumerator Setup()
         {
@@ -20,6 +22,9 @@ namespace Tests.PlayTests
                 AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Scenes/ThePit", LoadSceneMode.Additive);
                 yield return new WaitUntil(() => asyncLoad.isDone);
             }
+
+            if (_prince == null) _prince = GameObject.Find("Prince");
+            if (_enemy == null) _enemy = GameObject.Find("Enemy");
 
             yield return new EnterPlayMode();
         }
@@ -34,19 +39,40 @@ namespace Tests.PlayTests
         // A UnityTest behaves like a coroutine in PlayMode
         // and allows you to yield null to skip a frame in EditMode
         [UnityTest]
-        public IEnumerator AdvanceWithSwordTests()
+        public IEnumerator PrinceAdvanceWithSwordTest()
         {
             // Setup test.
+            _enemy.SetActive(false);
+            _prince.SetActive(true);
             float expected_distance = 0.4161f;
             string commandFile = @"Assets\Tests\TestResources\advanceWithSword";
-            GameObject princeGameObject = GameObject.Find("Prince");
-            Vector2 startPosition = princeGameObject.transform.position;
-            InputController inputController = princeGameObject.GetComponent<InputController>();
+            Vector2 startPosition = _prince.transform.position;
+            InputController inputController = _prince.GetComponent<InputController>();
             AccessPrivateHelper.SetPrivateField(inputController, "recordedCommandsFile", commandFile);
             AccessPrivateHelper.AccessPrivateMethod(inputController, "ReplayRecordedCommands");
             // Let movements perform.
             yield return new WaitForSeconds(3);
-            Vector2 endPosition = princeGameObject.transform.position;
+            Vector2 endPosition = _prince.transform.position;
+            float advancedDistance = Vector2.Distance(startPosition, endPosition);
+            float error = advancedDistance - expected_distance;
+            Assert.True(Math.Abs(error) < 0.01);
+        }
+        
+        [UnityTest]
+        public IEnumerator GuardAdvanceWithSwordTest()
+        {
+            // Setup test.
+            _prince.SetActive(false);
+            _enemy.SetActive(true);
+            float expected_distance = 0.4295f;
+            string commandFile = @"Assets\Tests\TestResources\advanceWithSword";
+            Vector2 startPosition = _enemy.transform.position;
+            InputController inputController = _enemy.GetComponent<InputController>();
+            AccessPrivateHelper.SetPrivateField(inputController, "recordedCommandsFile", commandFile);
+            AccessPrivateHelper.AccessPrivateMethod(inputController, "ReplayRecordedCommands");
+            // Let movements perform.
+            yield return new WaitForSeconds(3);
+            Vector2 endPosition = _enemy.transform.position;
             float advancedDistance = Vector2.Distance(startPosition, endPosition);
             float error = advancedDistance - expected_distance;
             Assert.True(Math.Abs(error) < 0.01);
