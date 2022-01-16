39194d8db1c097fe0f4f0ec3ddb8b6c3b6dd3139
dante-signal31 <dante.signal31@gmail.com>
2022-01-16 08:18:08 +0100

    I've signaled fighting phases with animation events.

--- Assets/Scripts/AnimationEventsForwarder.cs
+++ Assets/Scripts/AnimationEventsForwarder.cs
@@ -4,38 +4,47 @@ using UnityEngine;
 
 namespace Prince
 {
-    public class AnimationEventsForwarder : MonoBehaviour
-    {
-        [Header("WIRING:")]
-        [Tooltip("Needed to signal fighting events to stay in synch with animations.")]
-        [SerializeField] private FightingInteractions fightingInteractions;
-        
-        public void StrikeStart()
+        public class AnimationEventsForwarder : MonoBehaviour
         {
-        
-        }
+                [Header("WIRING:")]
+                [Tooltip("Needed to signal fighting events to stay in synch with animations.")]
+                [SerializeField]
+                private FightingInteractions fightingInteractions;
 
-        public void BlockingChanceEnded()
-        {
-        
-        }
+///////// My strike can be blocked.
+                public void StrikeStart()
+                {
 
-        public void StrikeHit()
-        {
-        
-        }
-    
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+                }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
+                public void BlockingChanceEnded()
+                {
 
+                }
+
+                public void StrikeHit()
+                {
+
+                }
+
+//////// I block my enemy attack and now I have chance to counter attack.
+                public void BlockSwordStarted()
+                {
+
+                }
+
+                public void CounterAttackChanceEnded()
+                {
+
+                }
+
+//////// My attack has been blocked but my enemy counterattacks, now I have an small chance to block him.        
+                public void BlockedSwordStarted()
+                {
+                }
+
+                public void CounterBlockSwordChanceEnded()
+                {
+                }
+        }
 }
