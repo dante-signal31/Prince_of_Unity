2f419c3aaf6fcfea48af2b03094fac584ab93c04
dante-signal31 <dante.signal31@gmail.com>
2022-01-29 11:55:42 +0100

    Guard attack implementation pending.

--- Assets/Tests/PlayTests/FightingTests.cs
+++ Assets/Tests/PlayTests/FightingTests.cs
@@ -37,6 +37,9 @@ namespace Tests.PlayTests
             yield return TestSceneManager.UnLoadScene(_currentScene);
         }
         
+        /// <summary>
+        /// Test guard moves backwards when hit by a sword.
+        /// </summary>
         [UnityTest]
         public IEnumerator GuardHitBySwordMovementTest()
         {
@@ -46,10 +49,8 @@ namespace Tests.PlayTests
             _prince.SetActive(true);
             _prince.transform.SetPositionAndRotation(_startPosition2.transform.position, Quaternion.identity);
             _enemy.transform.SetPositionAndRotation(_startPosition1.transform.position, Quaternion.identity);
-            // I dont want enemy to move by itself.
+            // I dont want enemy to move or defend by itself.
             _enemy.GetComponentInChildren<GuardController>().enabled = false;
-            // I don't want EnemyPursuer clutter logs.
-            // _enemy.GetComponentInChildren<EnemyPursuer>().enabled = false;
             yield return null;
             float expected_distance = 0.697f;
             string commandFile = @"Assets\Tests\TestResources\oneSwordHit";
@@ -64,6 +65,42 @@ namespace Tests.PlayTests
             float movedDistance = Vector2.Distance(startPosition, endPosition);
             float error = movedDistance - expected_distance;
             // Assert Guard has moved what we expected.
+            Assert.True(Math.Abs(error) < 0.08);
+            yield return null;
+        }
+        
+        /// <summary>
+        /// Test prince moves backwards when hit by a sword.
+        /// </summary>
+        [UnityTest]
+        public IEnumerator PrinceHitBySwordMovementTest()
+        {
+            // Setup test.
+            LogAssert.ignoreFailingMessages = true;
+            // // I dont want enemy to move or defend by itself.
+            // _enemy.GetComponentInChildren<GuardController>().enabled = false;
+            // // I don't want EnemyPursuer to clutter log.
+            // _enemy.GetComponentInChildren<EnemyPursuer>().enabled = false;
+            _enemy.GetComponentInChildren<GuardFightingProfile>().fightingProfile.boldness = 1;
+            _enemy.GetComponentInChildren<GuardFightingProfile>().fightingProfile.attack = 1;
+            _enemy.SetActive(true);
+            _prince.SetActive(true);
+            _prince.transform.SetPositionAndRotation(_startPosition2.transform.position, Quaternion.identity);
+            _enemy.transform.SetPositionAndRotation(_startPosition1.transform.position, Quaternion.identity);
+            yield return null;
+            float expected_distance = 0.68f;
+            string commandFile = @"Assets\Tests\TestResources\oneSwordHit";
+            Vector2 startPosition = _prince.transform.position;
+            InputController inputController = _enemy.GetComponent<InputController>();
+            yield return null;
+            AccessPrivateHelper.SetPrivateField(inputController, "recordedCommandsFile", commandFile);
+            AccessPrivateHelper.AccessPrivateMethod(inputController, "ReplayRecordedCommands");
+            // Let movements perform.
+            yield return new WaitForSeconds(3);
+            Vector2 endPosition = _prince.transform.position;
+            float movedDistance = Vector2.Distance(startPosition, endPosition);
+            float error = movedDistance - expected_distance;
+            // Assert Prince has moved what we expected.
             Assert.True(Math.Abs(error) < 0.08);
             yield return null;
         }
