79890dd135a3d917385d4ff52ea3c4829beb3496
dante-signal31 <dante.signal31@gmail.com>
2022-02-06 01:18:45 +0100

    AnimatorExtensions refactored to its own source code file.

---
+++ Assets/Scripts/AnimatorExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Prince
+{
+    /// <summary>
+    /// <p>By default animator triggers get enqueued if they are fired while in a state that cannot consume it. That
+    /// is not the behaviour I want. I'd like trigger to vanish after a frame independently they are consumed or not.</p>
+    ///
+    /// <p>To do that, a method extension over Animator is needed to implement an special trigger call.</p>
+    ///
+    /// <p>Solution got from: https://forum.unity.com/threads/mecanim-trigger-stays-down-queued.314742/</p>
+    /// </summary>
+    public static class AnimatorExtensions
+    {
+        /// <summary>
+        /// Set trigger just for one frame and reset it afterwards.
+        /// </summary>
+        /// <param name="anim">Animator our state machine is running on.</param>
+        /// <param name="coroutineRunner">MonoBehavior that is calling this method.</param>
+        /// <param name="trigger">Name of trigger we want to call.</param>
+        public static void SetTriggerOneFrame(this Animator anim, string trigger, MonoBehaviour coroutineRunner) {
+            coroutineRunner.StartCoroutine(TriggerOneFrame(anim, trigger));
+        }
+ 
+        private static IEnumerator TriggerOneFrame(Animator anim, string trigger) {
+            anim.SetTrigger(trigger);
+            yield return null;
+            if (anim != null) {
+                anim.ResetTrigger(trigger);
+            }
+        }
+    }
+}
--- Assets/Scripts/FightingInteractions.cs
+++ Assets/Scripts/FightingInteractions.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Unity.Plastic.Newtonsoft.Json.Serialization;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
+
 
 namespace Prince
 {
