9e8b8cd3f361e786cca93c15b01e624566385e3f
dante-signal31 <dante.signal31@gmail.com>
2022-03-12 12:07:34 +0100

    Added a behaviour to Idle state to reset stuck stop triggers.

---
+++ Assets/Scripts/ResetTrigger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Prince
+{
+    /// <summary>
+    /// Sometimes triggers get stuck. This behaviour cleans a trigger when an state is entered.
+    /// </summary>
+    public class ResetTrigger : StateMachineBehaviour
+    {
+        [Tooltip("Trigger to reset when this state is entered.")]
+        [SerializeField] private string triggerToReset;
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex)
+        {
+            animator.ResetTrigger(triggerToReset);
+        }
+
+        // public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo,
+        //     int layerIndex)
+        // {
+        // }
+        //
+        // public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo,
+        //     int layerIndex)
+        // {
+        // }
+        //
+        // public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo,
+        //     int layerIndex)
+        // {
+        // }
+        //
+        // public override void OnStateIK(Animator animator, AnimatorStateInfo stateInfo,
+        //     int layerIndex)
+        // {
+        // }
+    }
+}
