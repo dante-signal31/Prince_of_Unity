b001f169b945885061d01fafd9ecfda40ee339ba
dante-signal31 <dante.signal31@gmail.com>
2022-06-18 11:33:51 +0200

    First and untested draft for vibration sensing for falling ground.

---
+++ Assets/Scripts/VibrationsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prince
+{
+    /// <summary>
+    /// This component emit vibrations events depending on Prince states.
+    /// </summary>
+    public class VibrationsController : MonoBehaviour
+    {
+
+        public class VibrationEvent : EventArgs
+        {
+            public Vector3 SourcePosition { get; private set; }
+            
+            /// <param name="position">Where vibration event was originated</param>
+            public VibrationEvent(Vector3 position)
+            {
+                SourcePosition = position;
+            }
+        }
+
+        [Header("WIRING:")] 
+        [Tooltip("Needed to know current character state.")] 
+        [SerializeField] private CharacterStatus characterStatus;
+
+        public event EventHandler<VibrationEvent> Vibration;
+
+        private EventBus _eventBus;
+
+        /// <summary>
+        /// Trigger vibration event both locally and through event bus.
+        /// </summary>
+        private void TriggerVibrationEvent()
+        {
+            VibrationEvent eventArgs = new VibrationEvent(transform.root.position);
+            _eventBus.TriggerEvent(Vibration, eventArgs, this);
+        }
+        
+        private void Awake()
+        {
+            _eventBus = GameObject.Find("EventBus").GetComponentInChildren<EventBus>();
+            _eventBus.RegisterEvent(Vibration);
+        }
+
+        private void FixedUpdate()
+        {
+            switch (characterStatus.CurrentState)
+            {
+                case CharacterStatus.States.Landing:
+                case CharacterStatus.States.HardLanding:
+                case CharacterStatus.States.DeadByFall:
+                case CharacterStatus.States.VerticalJumpEnd:
+                    TriggerVibrationEvent();
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VibrationsSensor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prince
+{
+    /// <summary>
+    /// This component subscribes to vibrations events and makes falling ground tremble if vibration is near enough.
+    /// </summary>
+    public class VibrationsSensor : MonoBehaviour
+    {
+        [Header("WIRING:")] 
+        [Tooltip("Needed to send trembling signal when needed.")] 
+        [SerializeField] private Animator stateMachine;
+
+        [Header("CONFIGURATION:")] 
+        [Tooltip("Activation distance.")] 
+        [SerializeField] private float activationDistance;
+
+        private EventBus _eventBus;
+        private bool _eventRegisteringRetryNeeded;
+
+        private void Awake()
+        {
+            _eventBus = GameObject.Find("EventBus").GetComponentInChildren<EventBus>();
+            _eventBus.AddListener<VibrationsController.VibrationEvent>(OnVibration);
+        }
+
+        private void Start()
+        {
+            if (_eventRegisteringRetryNeeded)
+            {
+                _eventBus.AddListener<VibrationsController.VibrationEvent>(OnVibration);
+                _eventRegisteringRetryNeeded = false;
+            }
+        }
+
+        private void OnEnable()
+        {
+            try
+            {
+                _eventBus.AddListener<VibrationsController.VibrationEvent>(OnVibration);
+            }
+            catch (EventBus.NotExistingEvent ev)
+            {
+                _eventRegisteringRetryNeeded = true;
+            }
+        }
+
+        private void OnDisable()
+        {
+            _eventBus.RemoveListener<VibrationsController.VibrationEvent>(OnVibration);
+        }
+
+        /// <summary>
+        /// If vibration event takes place into activation distance then trigger a tremble signal to state machine.
+        /// </summary>
+        /// <param name="sender">Game object who triggered vibration event.</param>
+        /// <param name="eventArgs">Event data.</param>
+        private void OnVibration(object sender, VibrationsController.VibrationEvent eventArgs)
+        {
+            float distance = Vector3.Distance(transform.root.position, eventArgs.SourcePosition);
+            if (distance <= activationDistance)
+            {
+                stateMachine.SetTrigger("Tremble");
+            }
+        }
+        
+        // TODO: Draw a helper circle to show activation distance on editor.
+    }
+}
