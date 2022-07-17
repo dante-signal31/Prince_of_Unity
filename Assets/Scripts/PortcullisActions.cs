1f94831bec2d9292dbd952e765ed273c1a4b5c33
dante-signal31 <dante.signal31@gmail.com>
2022-07-17 02:22:56 +0200

    Now switches and portcullis draw a debugging line to each other at editor.

--- Assets/Scripts/PortcullisActions.cs
+++ Assets/Scripts/PortcullisActions.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
 namespace Prince
 {
@@ -15,6 +18,17 @@ namespace Prince
         [Tooltip("Needed to operate iron curtain.")] 
         [SerializeField] private Animator stateMachine;
 
+        [Header("DEBUG:")] 
+        [Tooltip("Color to apply to gizmos.")]
+        [SerializeField] private Color gizmoColor;
+        [Tooltip("Size for gizmos.")]
+        [SerializeField] private float gizmoSize;
+
+        #if UNITY_EDITOR
+        private List<GameObject> ClosingSwitchesList = new List<GameObject>();
+        private List<GameObject> OpeningSwitchesList = new List<GameObject>();
+        #endif
+        
         /// <summary>
         /// Start portcullis opening sequence.
         /// </summary>
@@ -38,5 +52,68 @@ namespace Prince
         {
             stateMachine.SetTrigger("CloseFast");
         }
+        
+        #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            UpdateClosingSwitchesList();
+            UpdateOpeningSwitchesList();
+        }
+
+        /// <summary>
+        /// Update list of switches that close this portcullis.
+        /// </summary>
+        private void UpdateClosingSwitchesList()
+        {
+            ClosingSwitchesList.Clear();
+            List<GameObject> closingSwitches = new List<GameObject>(GameObject.FindGameObjectsWithTag("ClosingSwitch"));
+            foreach (GameObject _switch in closingSwitches)
+            {
+                if (_switch.GetComponentInChildren<SwitchEvents>().Listeners.Contains(gameObject))
+                    ClosingSwitchesList.Add(_switch);
+            }
+        }
+
+        /// <summary>
+        /// Update list of switches that close this portcullis.
+        /// </summary>
+        private void UpdateOpeningSwitchesList()
+        {
+            OpeningSwitchesList.Clear();
+            List<GameObject> openingSwitches = new List<GameObject>(GameObject.FindGameObjectsWithTag("OpeningSwitch"));
+            foreach (GameObject _switch in openingSwitches)
+            {
+                if (_switch.GetComponentInChildren<SwitchEvents>().Listeners.Contains(gameObject))
+                    OpeningSwitchesList.Add(_switch);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            DrawConnectionsToSwitches();
+        }
+
+        private void DrawConnectionsToList(List<GameObject> listToConnect)
+        {
+            if (listToConnect.Count == 0) return;
+            gizmoColor.a = 1;
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawSphere(transform.position, gizmoSize / 2);
+            foreach (GameObject _switch in listToConnect)
+            {
+                Color switchGizmoColor = _switch.GetComponentInChildren<SwitchEvents>().GizmoColor;
+                switchGizmoColor.a = 1;
+                Gizmos.color = switchGizmoColor;
+                Gizmos.DrawCube(_switch.transform.position, new Vector3(gizmoSize, gizmoSize));
+                Gizmos.DrawLine(transform.position, _switch.transform.position);
+            }
+        }
+
+        private void DrawConnectionsToSwitches()
+        {
+            DrawConnectionsToList(ClosingSwitchesList);
+            DrawConnectionsToList(OpeningSwitchesList);
+        }
+        #endif
     }
 }
--- Assets/Scripts/SwitchEvents.cs
+++ Assets/Scripts/SwitchEvents.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -16,7 +17,27 @@ namespace Prince
         [Header("CONFIGURATION:")]
         [Tooltip("Event launched by this brick when it is activated.")]
         [SerializeField] private UnityEvent activated;
+        
+        [Header("DEBUG:")]
+        [Tooltip("Show this component logs on console window.")]
+        [SerializeField] private bool showLogs;
+        [Tooltip("Color used to draw debug gizmo.")]
+        [SerializeField] private Color gizmoColor;
+        [Tooltip("Size for starting and ending gizmo.")]
+        [SerializeField] private float gizmoSize;
+        
+        #if UNITY_EDITOR
+        /// <summary>
+        /// List of listeners that have subscribed to this switch.
+        /// </summary>
+        public List<GameObject> Listeners { get; private set; }
 
+        /// <summary>
+        /// Color used to paint gizmos related to this game object.
+        /// </summary>
+        public Color GizmoColor => gizmoColor;
+        #endif
+        
         private bool _eventAlreadyTriggered;
         private void Update()
         {
@@ -30,5 +51,50 @@ namespace Prince
                 _eventAlreadyTriggered = false;
             }
         }
+
+        #if UNITY_EDITOR
+        /// <summary>
+        /// Update list of game object registered as listeners of activated event.
+        /// </summary>
+        private void UpdateListenerList()
+        {
+            Listeners.Clear();
+            int listenerCount = activated.GetPersistentEventCount();
+            this.Log($"(SwitchEvents - {transform.root.name}) There are {listenerCount} listeners.", showLogs);
+            for (int i=0; i<listenerCount; i++)
+            {
+                this.Log($"(SwitchEvents - {transform.root.name}) Listener {i} is of type {activated.GetPersistentTarget(i).GetType()}.", showLogs);
+                MonoBehaviour listener = (MonoBehaviour) activated.GetPersistentTarget(i);
+                Listeners.Add(listener.gameObject);
+            }
+        }
+        
+        private void OnDrawGizmosSelected()
+        {
+            DrawConnectionsToListeners();
+        }
+
+        private void DrawConnectionsToListeners()
+        {
+            if (Listeners.Count == 0) return;
+            gizmoColor.a = 1;
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawCube(transform.position, new Vector3(gizmoSize, gizmoSize));
+            this.Log($"(SwitchEvents - {transform.root.name}) Drawing connections with {Listeners.Count} listeners.", showLogs);
+            foreach (GameObject listener in Listeners)
+            {
+                this.Log(
+                    $"(SwitchEvents - {transform.root.name}) Drawing connections with listener from {transform.position} to {listener.transform.position}.",
+                    showLogs);
+                Gizmos.DrawSphere(listener.transform.position, gizmoSize / 2);
+                Gizmos.DrawLine(transform.position, listener.transform.position);
+            }
+        }
+
+        private void OnValidate()
+        {
+            UpdateListenerList();
+        }
+        #endif
     }
 }
