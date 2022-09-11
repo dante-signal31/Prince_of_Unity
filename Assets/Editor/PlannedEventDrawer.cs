a1a2b3186d4ef5c3307b2fdd835e5e8eb464fee4
dante-signal31 <dante.signal31@gmail.com>
2022-09-11 19:27:27 +0200

    First working draft for PlannedEvent property drawer.

---
+++ Assets/Editor/PlannedEventDrawer.cs
@@ -0,0 +1,92 @@
+﻿using Prince;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Editor
+{
+    [UnityEditor.CustomPropertyDrawer(typeof(GameTimer.PlannedEvent))]
+    public class PlannedEventDrawer : UnityEditor.PropertyDrawer
+    {
+        private float _lineCount;
+        private float _initialHeight;
+        readonly float _lineHeight = EditorGUIUtility.singleLineHeight;
+        private float CurrentHeight => _initialHeight + (_lineCount * (_lineHeight + EditorGUIUtility.standardVerticalSpacing));
+        
+        public override void OnGUI(Rect position, UnityEditor.SerializedProperty property,
+            UnityEngine.GUIContent label)
+        {
+            // Using BeginProperty / EndProperty on the parent property means that
+            // prefab override logic works on the entire property.
+            EditorGUI.BeginProperty(position, label, property);
+            _lineCount = 0;
+            // Draw label.
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            int originalIndent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            
+            // Calculate rects. I want to show first trigerringType combo and after a property field showing
+            // only the one that is needed for this trigerring type.
+            _initialHeight = position.y;
+            Rect triggeringTypeRect = new Rect(position.x, CurrentHeight, position.width, _lineHeight);
+            _lineCount++;
+            Rect triggeringTimeRect = new Rect(position.x, CurrentHeight, position.width, _lineHeight);
+            _lineCount++;
+
+            // Keep track on any changes in this inspector values.
+            EditorGUI.BeginChangeCheck();
+            
+            // Draw fields.
+            // Show triggering time.
+            SerializedProperty serializedTriggeringType = property.FindPropertyRelative("triggeringType");
+            GameTimer.TriggeringTimeTypes selectedTimeType = (GameTimer.TriggeringTimeTypes) serializedTriggeringType.enumValueIndex;
+            selectedTimeType = (GameTimer.TriggeringTimeTypes) EditorGUI.EnumPopup(triggeringTypeRect, serializedTriggeringType.displayName, selectedTimeType);
+            serializedTriggeringType.enumValueIndex = (int) selectedTimeType;
+            EditorGUI.indentLevel++;
+            switch (selectedTimeType)
+            {
+                case GameTimer.TriggeringTimeTypes.ElapsedSeconds:
+                    SerializedProperty serializedElapsedSeconds = property.FindPropertyRelative("elapsedSeconds");
+                    EditorGUI.PropertyField(triggeringTimeRect, serializedElapsedSeconds);
+                    break;
+                case GameTimer.TriggeringTimeTypes.GameTotalTimePercentage:
+                    SerializedProperty serializedElapsedPercentage = property.FindPropertyRelative("elapsedPercentage");
+                    EditorGUI.Slider(triggeringTimeRect, serializedElapsedPercentage, 0, 100);
+                    break;
+            }
+            EditorGUI.indentLevel--;
+            // Now show triggered event.
+            SerializedProperty serializedEventToTrigger = property.FindPropertyRelative("eventToTrigger");
+            
+            Rect eventsRect = new Rect(position.x, CurrentHeight, position.width, EditorGUI.GetPropertyHeight(serializedEventToTrigger, includeChildren: true));
+            EditorGUI.PropertyField(eventsRect, property.FindPropertyRelative("eventToTrigger"), includeChildren: true);
+            _lineCount += EditorGUI.GetPropertyHeight(serializedEventToTrigger) / _lineHeight;
+            // Set indent back to what it was.
+            EditorGUI.indentLevel = originalIndent;
+            // Store any change.
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.serializedObject.ApplyModifiedProperties();
+            }
+            EditorGUI.EndProperty();
+        }
+
+        // Needed to return global property size to parents like lists drawers.
+        public override float GetPropertyHeight(UnityEditor.SerializedProperty property, UnityEngine.GUIContent label)
+        {
+            // Here I use my own local lineCount because if I use global _lineCount it gets messy at lists
+            // with multiple items.
+            
+            // First the line count for the static part: triggeringType and triggeringTime.
+            float lineCount = 2;
+            // Now calculate dynamic part.
+            SerializedProperty serializedEventToTrigger = property.FindPropertyRelative("eventToTrigger");
+            lineCount += EditorGUI.GetPropertyHeight(serializedEventToTrigger) / _lineHeight;
+            
+            // With total line count, now you can get total property height.
+            // Vertical spacing is multiplied by three because we have 3 vertical element that need
+            // spacing between them: triggeringType, triggeringTime and eventToTrigger.
+            return lineCount * _lineHeight + 3 * EditorGUIUtility.standardVerticalSpacing;
+        }
+    }
+}
---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Prince
+{
+    /// <summary>
+    /// Game component to keep track of time passed and launch events at specified moments.
+    /// </summary>
+    public class GameTimer : MonoBehaviour
+    {
+        public enum TriggeringTimeTypes
+        {
+            ElapsedSeconds,
+            GameTotalTimePercentage
+        }
+
+        /// <summary>
+        /// Event to be launched at specific moments.
+        /// </summary>
+        [Serializable]
+        public class PlannedEvent: IComparable<PlannedEvent>
+        {
+            [Tooltip("What we want to set, whether seconds or game percentage.")]
+            public TriggeringTimeTypes triggeringType;
+            [Tooltip("Second to let pass since the beginning to trigger this event.")]
+            public float elapsedSeconds;
+            [Tooltip("Game percentage to let pass before triggering this event.")]
+            [Range(0.0f, 100.0f)]
+            public float elapsedPercentage;
+            [Tooltip("Event to trigger when elapsed seconds are reached.")] 
+            public UnityEvent eventToTrigger;
+
+            public int CompareTo(PlannedEvent other)
+            {
+                return elapsedSeconds.CompareTo(other.elapsedSeconds);
+            }
+        }
+
+        [Header("WIRING:")] 
+        [Tooltip("Needed to know game total time.")]
+        [SerializeField] private GameConfiguration gameConfiguration;
+        
+        [Header("CONFIGURATION:")] 
+        [Tooltip("List of events to trigger at specified moments.")]
+        [SerializeField] private List<PlannedEvent> plannedEvents;
+
+        private int _eventIndex;
+        
+        private void Awake()
+        {
+            plannedEvents.Sort();
+        }
+        
+        /// <summary>
+        /// Elapsed time since the beginning of game.
+        /// </summary>
+        public float ElapsedSeconds { get; private set; }
+
+        private void FixedUpdate()
+        {
+            ElapsedSeconds += Time.deltaTime;
+            if (plannedEvents[_eventIndex].elapsedSeconds <= ElapsedSeconds)
+            {
+                if (plannedEvents[_eventIndex].eventToTrigger != null)
+                {
+                    plannedEvents[_eventIndex].eventToTrigger.Invoke();
+                }
+            }
+        }
+
+        private void OnValidate()
+        {
+            plannedEvents.Sort();
+        }
+    }
+}
