8e3fb558664b3acb1ebd97f02188eff235ff4c06
dante-signal31 <dante.signal31@gmail.com>
2022-12-04 01:20:03 +0100

    TODO solved. Now mouse cursor is hidden when game is started.

---
+++ Assets/Scripts/MouseHider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prince
+{
+    // This component hides mouse cursor when game starts
+    public class MouseHider : MonoBehaviour
+    {
+        private void Start()
+        {
+            Cursor.visible = false;
+        }
+    }
+}
