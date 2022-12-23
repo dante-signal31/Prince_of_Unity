8602e543e714496ad11d0f2be6b89aa26581476f
dante-signal31 <dante.signal31@gmail.com>
2022-12-23 20:41:20 +0100

    TODO "solved", I don't know why I cannot reproduce error again. It seems error happens when I update unity engine but after a while it fixes itself.

--- Assets/Tests/PlayTests/FallingGroundTests.cs
+++ Assets/Tests/PlayTests/FallingGroundTests.cs
@@ -514,7 +514,6 @@ namespace Tests.PlayTests
         [UnityTest]
         public IEnumerator PrinceFallsWithFallingGroundTest()
         {
-            // TODO: Sometimes Prince survives when falls in this test. Must fix it.
             _cameraController.PlaceInRoom(_room10);
             _enemy.SetActive(false);
             _prince.SetActive(true);
