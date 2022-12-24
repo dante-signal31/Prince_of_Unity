09c8490b175feb5e8fcd73283c7bc9fbc818daf1
dante-signal31 <dante.signal31@gmail.com>
2022-12-24 13:22:42 +0100

    TODO added.

--- Assets/Scripts/CharacterStatus.cs
+++ Assets/Scripts/CharacterStatus.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 namespace Prince
 {
+    /// TODO: Guards components stay active after he dies. Must fix it.
     /// <summary>
     /// This component keeps every variable about Character general state.
     /// </summary>
