f2bbe600c844bee8e1d394d3bcb2f06d5408f1c7
dante-signal31 <dante.signal31@gmail.com>
2022-12-25 11:57:29 +0100

    TODO solved. Now falling sliding animation is not triggered when character touches a wall with his back while falling.

--- Assets/Scripts/WallSensor.cs
+++ Assets/Scripts/WallSensor.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 namespace Prince
 {
-    // TODO: Sometimes when falling character touches wall with his back, so it activates slide falling animation which looks odd. Must fix it. 
     /// <summary>
     /// This component detects when Prince touches a wall.
     ///
@@ -15,6 +14,8 @@ namespace Prince
         [Header("WIRING:")]
         [Tooltip("Needed to signal we are touching a wall.")]
         [SerializeField] private Animator stateMachine;
+        [Tooltip("Needed to know if we are looking rightwards and our current state.")]
+        [SerializeField] private CharacterStatus characterStatus;
         
         [Header("DEBUG:")]
         [Tooltip("Show this component logs on console window.")]
@@ -36,6 +37,9 @@ namespace Prince
         {
             if (col.gameObject.layer == _architectureLayer)
             {
+                // Sometimes, when falling character touches wall with his back, it activates slide falling animation
+                // which looks odd. To fix it I check if touched collider is in our back and we are falling. 
+                if (IamInAnyFallingState() && !ColliderInFrontOfMe(col)) return;
                 stateMachine.SetBool("TouchingWall", true);
                 isTouchingWall = true;
                 this.Log($"(WallSensor - {transform.root.name}) Touching a wall.", showLogs);
@@ -49,6 +53,35 @@ namespace Prince
                 stateMachine.SetBool("TouchingWall", false);
                 isTouchingWall = false;
                 this.Log($"(WallSensor - {transform.root.name}) No longer touching a wall.", showLogs);
+            }
+        }
+
+        /// <summary>
+        /// Whether touched collider is in front of character. 
+        /// </summary>
+        /// <param name="col">Touched collider.</param>
+        /// <returns>True if touched collider is in front of character. False instead.</returns>
+        private bool ColliderInFrontOfMe(Collider2D col)
+        {
+            float colliderPositionX = col.transform.position.x;
+            float currentPositionX = transform.position.x;
+            return ((colliderPositionX >= currentPositionX) && characterStatus.LookingRightWards) ||
+                   ((colliderPositionX < currentPositionX) && !characterStatus.LookingRightWards);
+        }
+
+        /// <summary>
+        /// Whether we are in any falling state.
+        /// </summary>
+        /// <returns>True if we are in any falling state. False instead.</returns>
+        private bool IamInAnyFallingState()
+        {
+            switch (characterStatus.CurrentState)
+            {
+                case CharacterStatus.States.Falling:
+                case CharacterStatus.States.FallStart:
+                    return true;
+                default:
+                    return false;
             }
         }
     }
