150155d429ef459e9478f27060192ed4c934f945
dante-signal31 <dante.signal31@gmail.com>
2023-04-30 12:34:19 +0200

    Scripts refactored to make their lines shorter.

--- Packaging/Linux/postuninst.sh
+++ Packaging/Linux/postuninst.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+desktop_launcher=ninja.dlab.Prince_of_Unity.desktop
+
 # Remove link in /usr/games
 rm /usr/games/Prince_of_Unity
 
 # Remove desktop entry.
-rm /usr/share/applications/ninja.dlab.Prince_of_Unity.desktop
+rm /usr/share/applications/$desktop_launcher
