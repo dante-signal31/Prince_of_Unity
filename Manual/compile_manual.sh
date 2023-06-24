b70cadb3d1f1779c094c96778a94242d59331504
dante-signal31 <dante.signal31@gmail.com>
2023-06-25 01:16:22 +0200

    Manual generation finally works.

--- Manual/compile_manual.sh
+++ Manual/compile_manual.sh
@@ -6,4 +6,7 @@ pandoc --listings -f markdown_mmd -o output/manual.tex output/manual.mmd
 ./populate_tex_template.py
 # Convert latex document to PDF.
 pdflatex -shell-escape -output-directory pdf output/PoU_manual.tex 
+# You need to run pdflatex twice to generate a proper table of contents.
+# Odd but true: https://tex.stackexchange.com/questions/314796/why-is-there-no-content-under-the-contents-section-generated-with-tableofconten
+pdflatex -shell-escape -output-directory pdf output/PoU_manual.tex 
 
--- Manual/populate_tex_template.py
+++ Manual/populate_tex_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import pathlib
 
-from jinja2 import Template
+from jinja2 import Template, TemplateSyntaxError
 
 content_to_input = "output/manual.tex"
 latex_template = "template/complete_manual_template.tex"
@@ -11,4 +11,12 @@ if __name__ == '__main__':
     content = pathlib.Path(content_to_input).read_text()
     template = pathlib.Path(latex_template).read_text()
     with open(file=populated_latex, mode="w") as complete_manual:
-        complete_manual.write(Template(template).render(contents=content))
+        try:
+            manual_template = Template(template)
+            complete_manual_content = manual_template.render(contents=content)
+        except TemplateSyntaxError as e:
+            print(
+                "Jinja2 template syntax error during render of line {}:{} error: {}".
+                format(e.name, e.lineno, e.message))
+            raise e
+        complete_manual.write(complete_manual_content)
